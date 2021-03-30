ff7627529f5044a3b838a96294953552b7b8062c
Erik Hoffman <erik.m.hoffman@gmail.com>
2021-03-29 21:09:47 -0400

    Pull javascript into its own file.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var asset = "bitcoin"
+function changeAsset(newAsset){
+    asset = newAsset;
+    getPrice(asset);
+    console.log(asset);
+}
+async function getPrice (asset){
+    url = 'https://api.coincap.io/v2/assets/' + asset;
+    const response = await fetch(url);
+    const myJson = response.json().then(data => {
+        var data = Array(data);
+        changePercent = data[0]['data']['changePercent24Hr'];
+        var price = parseFloat(data[0]['data']['priceUsd']).toFixed(2);
+        if (changePercent < 0){
+            var change = " <div style='color:red' class='fa fa-arrow-circle-down'> <span class='asset'>" + parseFloat(changePercent).toFixed(2) + "%" + "</span></div>";
+        }
+        else{
+            var change = " <div style='color:green' class='fa fa-arrow-circle-up'> <span class='asset'>" + parseFloat(changePercent).toFixed(2) + "%" + "</span></div>";
+        }
+        document.getElementById("ticker").innerHTML = "$" + price.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        document.getElementById("asset").innerHTML = asset + change;
+    })
+}
+function startTimer() {
+    timer = setInterval(function() { 
+        getPrice(asset);
+    }, 5000);
+}
+getPrice(asset);
+startTimer();
